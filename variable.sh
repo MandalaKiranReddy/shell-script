7934f0d1c088caa9d1165eb6df7f5d1a9a1c6552
MandalaKiranReddy <mkiranreddy7558@gmail.com>
2024-12-30 20:55:47 +0530

    added one more file

---
+++ variable.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+echo "Rahim: Hi Robert"
+echo "Robert: Hello Rahim"
+echo "Rahim: how are you doing"
+echo "Robert: I am find How about you"
+echo "Rahim: I am good. Thanks for asking"
