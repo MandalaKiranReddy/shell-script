196dee42c5c6c20b1c65a9f726b3260148e48eff
MandalaKiranReddy <mkiranreddy7558@gmail.com>
2025-01-01 21:28:44 +0530

    updated the installtion file

--- installation.sh
+++ installation.sh
@@ -2,6 +2,10 @@
 
 USER_NAME=$USER
 
+echo "username is : $USER_NAME"
+echo "username is : $(USER_NAME)"
+echo "username is : ${USER_NAME}"
+
 if [ $USER_NAME -eq "ROOT"]
 then
    dnf install mysql -y
