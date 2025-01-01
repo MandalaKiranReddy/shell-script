2c8fac3b2439a67814f89f5c175484afb3a11f60
MandalaKiranReddy <mkiranreddy7558@gmail.com>
2025-01-01 16:45:00 +0530

    updated datatypes

--- datatypes.sh
+++ datatypes.sh
@@ -3,6 +3,8 @@
 NUMBER1=$1
 NUMBER2=$2
 
-SUM= $($NUMBER1+$NUMBER2)
+echo "TimeStamp for adding two numbers : $(date)"
+
+SUM= $(($NUMBER1+$NUMBER2))
 
 echo "Sum of two numbers:$NUMBER1+$NUMBER2=$SUM"
