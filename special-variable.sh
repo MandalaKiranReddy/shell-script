9cccf169a60a557d8700191d3d1ae77497f32253
MandalaKiranReddy <mkiranreddy7558@gmail.com>
2025-01-01 20:40:07 +0530

    updated the special variables

--- special-variable.sh
+++ special-variable.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "First Variable : $1"
 echo "All Variables Passed : $@"
 echo "Number of Variables Passed : $#"
 echo "Script Name : $0"
@@ -7,5 +8,5 @@ echo "Which User Running Script:$USER"
 echo "Present Working Directory: $PWD"
 echo "Present user home directory: $HOME"
 echo "ProcessId of the Current Process $$"
-sleep 60
+sleep 60 &
 echo "ProcessId of the background process $!"
